9f5f04e708549212cf5db6e550e9bc7ab20207c3
TheoKlein <theokleintw@gmail.com>
2016-04-09 19:29:21 +0800

    20160409 Update

---
+++ UVa題庫/c044.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    int n , i , j , max = 0;
+    char c;
+    int frequency[26] = { 0 };
+    scanf("%d%c",&n,&c);
+    while(n-- != 0){
+        char input[100];
+        gets(input);
+        unsigned long int length = strlen(input);
+        for(i = 0 ; i < length ; ++i){
+            if(isalpha(input[i])){
+                frequency[(toupper(input[i])) - 65]++;
+                if(frequency[(toupper(input[i])) - 65] > max)
+                    max = frequency[(toupper(input[i])) - 65];
+            }
+        }
+    }
+    for(i = max ; i > 0 ; --i){
+        for(j = 0 ; j < 26 ; ++j){
+            if(frequency[j] == i)
+                printf("%c %d\n",j + 65 , frequency[j]);
+        }
+    }
+    return 0;
+}
