4c07773a884e8db67ec1a3538fd1757a15105aaa
TheoKlein <theokleintw@gmail.com>
2016-04-09 22:03:30 +0800

    20160409 Update

---
+++ 基礎題庫/d058.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf ("%d",&n);
+    //true = 1 , false = 0
+    printf ("%d\n",(n > 0) - (n < 0));
+    
+    return 0;
+}
---
+++ 基礎題庫/d060.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int m;
+    scanf ("%d",&m);
+    printf ("%d\n",(85 - m) % 60);
+    
+    return 0;
+}
