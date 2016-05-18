495363771d652502ffe1e7fc55495edb1a3be201
TheoKlein <404262339@mail.fju.edu.tw>
2016-05-18 17:42:50 +0800

    Update c014.cpp

--- UVa題庫/c014.cpp
+++ UVa題庫/c014.cpp
@@ -9,9 +9,9 @@ int main() {
         int num1[10] = {0}, num2[10]  = {0}, count = 0;
         if(input1 == "0" && input2 == "0")
             break;
-        for(long int i = input1.length() - 1  , j = 0 ; i >= 0 ; --i , ++j)
+        for(long int i = input1.length() - 1 , j = 0 ; i >= 0 ; --i , ++j)
             num1[i] = input1[j] - 48;
-        for(long int i = input2.length() - 1  , j = 0 ; i >= 0 ; --i , ++j)
+        for(long int i = input2.length() - 1 , j = 0 ; i >= 0 ; --i , ++j)
             num2[i] = input2[j] - 48;
         
         for(int i = 0 ; i < 10 ; ++i){
