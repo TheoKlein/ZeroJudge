84df2b714ff5ae9417d81efe9adf81eab064364b
TheoKlein <theokleintw@gmail.com>
2016-07-18 19:45:18 +0800

    20160718 Update

---
+++ UVa題庫/c045.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int index = 0;
+    unsigned long int max = 0;
+    string input;
+    char array[100][100] = {' ' , ' '};
+    while(getline(cin , input)){
+        for(int i = 0 ; i < input.length() ; ++i)
+            array[index][i] = input[i];
+        if(input.length() > max)
+            max = input.length();
+        index++;
+    }
+    index--;
+    for(int i = 0 ; i < max ; ++i){
+        for(int j = index ; j >= 0 ; --j){
+            if(array[j][i] == '\0')
+                cout << ' ';
+            else
+                cout << array[j][i];
+        }
+        cout << endl;
+    }
+}
+
